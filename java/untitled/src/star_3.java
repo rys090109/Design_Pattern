63939a88df6756585dab045ef3a69b30bd14b0e4
노윤서 <rys090108@gmail.com>
2026-05-19 17:14:38 +0900

    커밋

---
+++ java/untitled/src/star_3.java
@@ -0,0 +1,10 @@
+public class star_3 {
+    public static void main(String[] args) {
+        for(int i = 0; i < 5; i++) {
+            for(int j = 0; j < 4 - i; j++) {
+                System.out.print(" ");
+            }
+            System.out.println("*");
+        }
+    }
+}
---
+++ java/untitled/src/star_4.java
@@ -0,0 +1,2 @@
+public class star_4 {
+}
